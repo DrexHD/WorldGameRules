acdcd55795364b171ea4743bfc400aff7889f8a7
Drex <nicknamedrex@gmail.com>
2024-06-28 02:59:07 +0200

    Enable mod in singleplayer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,5 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.1.0] - 2024-06-28
+### Changed
+- WorldGameRules can now be used in singleplayer
+
 ## [1.0.1] - 2024-06-19
 Initial release
--- src/main/java/me/drex/world_gamerules/WorldGameRulesMod.java
+++ src/main/java/me/drex/world_gamerules/WorldGameRulesMod.java
@@ -2,12 +2,13 @@ package me.drex.world_gamerules;
 
 import me.drex.world_gamerules.command.WorldGameRuleCommand;
 import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
-public class WorldGameRulesMod implements DedicatedServerModInitializer {
+public class WorldGameRulesMod implements ModInitializer {
 
     @Override
-    public void onInitializeServer() {
+    public void onInitialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> WorldGameRuleCommand.register(dispatcher));
     }
 
